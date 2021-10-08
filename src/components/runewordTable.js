42b00b29b940201f844fd57e90fbe52d4ecab909
codingLogan <reachCodingLogan@gmail.com>
2021-10-07 20:33:51 -0600

    Add sortable table

---
+++ src/components/runewordTable.js
@@ -0,0 +1,95 @@
+/**
+ *
+ * @param {Object} dataColumns - {wordKey: label...}
+ */
+export default function runewordTable(
+  runewords,
+  dataColumns = { name: "Name", minLevelForRune: "Level", sockets: "Sockets" },
+  displayOptions = {}
+) {
+  const wordTable = document.createElement("table");
+  const headerRow = document.createElement("tr");
+  Object.keys(dataColumns).map((key) => {
+    const headerColumn = document.createElement("th");
+    headerColumn.innerText = dataColumns[key];
+    headerRow.appendChild(headerColumn);
+  });
+
+  // Set up the table and header row
+  wordTable.appendChild(headerRow);
+
+  // Render the data rows
+  runewords.forEach((word) => {
+    const wordRow = document.createElement("tr");
+
+    Object.keys(dataColumns).map((key) => {
+      const dataColumn = document.createElement("td");
+      dataColumn.innerText = word[key];
+      wordRow.appendChild(dataColumn);
+    });
+
+    wordTable.appendChild(wordRow);
+  });
+
+  return wordTable;
+}
+
+let tableContainer = null;
+let currentRunewords = [];
+
+export function renderTableTo(
+  containerElement,
+  runewords,
+  dataColumns,
+  displayOptions
+) {
+  tableContainer = containerElement;
+  currentRunewords = runewords;
+
+  // Render the actions and the table below it
+  containerElement.appendChild(tableActions());
+  containerElement.appendChild(
+    runewordTable(runewords, dataColumns, displayOptions)
+  );
+}
+
+export function createActionButton(text, onClick) {
+  const actionButton = document.createElement("button");
+  actionButton.innerText = text;
+  actionButton.onclick = onClick;
+  return actionButton;
+}
+
+function clearTable(tableContainerNode) {
+  while (tableContainerNode.firstChild) {
+    tableContainerNode.removeChild(tableContainerNode.firstChild);
+  }
+}
+
+function sortTable(propertyName) {
+  currentRunewords.sort((a, b) => {
+    if (a[propertyName] === b[propertyName]) {
+      return 0;
+    }
+
+    return a[propertyName] < b[propertyName] ? -1 : 1;
+  });
+
+  clearTable(tableContainer);
+  renderTableTo(tableContainer, currentRunewords);
+}
+
+export function tableActions() {
+  const actionDiv = document.createElement("div");
+
+  const buttons = [
+    createActionButton("Name", () => sortTable("name")),
+    createActionButton("Level", () => sortTable("minLevelForRune")),
+  ];
+
+  buttons.forEach((button) => {
+    actionDiv.appendChild(button);
+  });
+
+  return actionDiv;
+}
--- src/pages/runewords.js
+++ src/pages/runewords.js
@@ -1,49 +1,11 @@
 import { getRuneWords } from "../data/runeWords.js";
+import runewordTable, {
+  renderTableTo,
+  tableActions,
+} from "../components/runewordTable.js";
 
 const runewords = getRuneWords();
 const runewordsSection = document.getElementById("runewords");
 
-// Create a div per runeword
-function renderDivs() {
-  runewords.forEach((runeword) => {
-    const runewordDiv = document.createElement("div");
-    runewordDiv.innerText = runeword.name;
-    runewordsSection.append(runewordDiv);
-  });
-}
-
-/**
- *
- * @param {Object} dataColumns - {wordKey: label...}
- */
-function renderTable(
-  dataColumns = { name: "Name", minLevelForRune: "Level", sockets: "Sockets" }
-) {
-  const wordTable = document.createElement("table");
-  const headerRow = document.createElement("tr");
-  Object.keys(dataColumns).map((key) => {
-    const headerColumn = document.createElement("th");
-    headerColumn.innerText = dataColumns[key];
-    headerRow.appendChild(headerColumn);
-  });
-
-  // Set up the table and header row
-  wordTable.appendChild(headerRow);
-  runewordsSection.appendChild(wordTable);
-
-  // Render the data rows
-  runewords.forEach((word) => {
-    const wordRow = document.createElement("tr");
-
-    Object.keys(dataColumns).map((key) => {
-      const dataColumn = document.createElement("td");
-      dataColumn.innerText = word[key];
-      wordRow.appendChild(dataColumn);
-    });
-
-    wordTable.appendChild(wordRow);
-  });
-}
-
-// renderDivs();
-renderTable();
+// Render the table of runewords
+renderTableTo(runewordsSection, runewords);
