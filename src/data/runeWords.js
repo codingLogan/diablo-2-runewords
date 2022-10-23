0c6625a18a1021ba100dcaedd1cd2907ee9ee5b2
Logan Rasmussen <logan.razclan1@gmail.com>
2022-10-23 11:16:06 -0600

    Add runewords from D2R patch 2.4

--- src/data/runeWords.js
+++ src/data/runeWords.js
@@ -2,12 +2,13 @@ import wordsOriginal from "./original-words.js";
 import words110 from "./1.10-words.js";
 import words110Ladder from "./1.10-ladder-words.js";
 import words111 from "./1.11-words.js";
+import d2r24 from "./d2r-2.4-words.js";
 
 import calculateMinLevel from "../utilities/calculate-level.js";
 
 // Combine all runewords into onelist
 export function getRuneWords() {
-  return [...wordsOriginal, ...words110, ...words110Ladder, ...words111].map(
+  return [...wordsOriginal, ...words110, ...words110Ladder, ...words111, d2r24].map(
     (runeword) => ({
       ...runeword,
       minLevelForRune: calculateMinLevel(runeword.runeOrder),
