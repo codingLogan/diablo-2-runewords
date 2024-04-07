95f658205c62553fe868cbe895b384389c7f5b2d
Logan Rasmussen <reachCodingLogan@gmail.com>
2024-04-07 15:05:46 -0600

    Add pwa manifest (#8)
    * Update the code for PWA functionality
    
    * Build the new site files

---
+++ src/serviceWorker.js
@@ -0,0 +1,45 @@
+const addResourcesToCache = async (resources) => {
+    const cache = await caches.open("v1");
+    await cache.addAll(resources);
+};
+  
+try {
+    let files = [
+        "/index.html",
+        "/runes.html",
+        "/serviceWorkerApp.js"
+    ]
+
+    const basePath = '/diablo-2-runewords'
+
+    files = files.map(file => basePath + file)
+
+    self.addEventListener("install", (event) => {
+        event.waitUntil(
+            addResourcesToCache(files),
+        );
+    });
+
+    const putInCache = async (request, response) => {
+        const cache = await caches.open("v1");
+        await cache.put(request, response);
+    };
+      
+    const cacheFirst = async (request) => {
+        const responseFromCache = await caches.match(request);
+        if (responseFromCache) {
+          return responseFromCache;
+        }
+        const responseFromNetwork = await fetch(request);
+        putInCache(request, responseFromNetwork.clone());
+        return responseFromNetwork;
+    };
+      
+    self.addEventListener("fetch", (event) => {
+        event.respondWith(cacheFirst(event.request));
+    });
+      
+} catch(e) {
+    console.error("Error", e)
+}
+  
