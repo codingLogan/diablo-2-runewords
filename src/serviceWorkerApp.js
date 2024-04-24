5dbd8dd1301fe3357c695f98b8365707b5f314e7
codingLogan <reachCodingLogan@gmail.com>
2024-04-23 19:02:41 -0600

    Update service worker scope

--- src/serviceWorkerApp.js
+++ src/serviceWorkerApp.js
@@ -1,9 +1,7 @@
 const registerServiceWorker = async () => {
     if ("serviceWorker" in navigator) {
       try {
-        const registration = await navigator.serviceWorker.register(new URL('./serviceWorker.js', import.meta.url), {
-          scope: "/docs/",
-        });
+        const registration = await navigator.serviceWorker.register(new URL('./serviceWorker.js', import.meta.url));
         if (registration.installing) {
           console.log("Service worker installing");
         } else if (registration.waiting) {
