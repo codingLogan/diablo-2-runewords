4eabf3daea1ff458ef66385348184309e857dea9
codingLogan <reachCodingLogan@gmail.com>
2024-10-19 13:47:45 -0600

    Update tests for working functionality

--- src/runewords/runewords.test.js
+++ src/runewords/runewords.test.js
@@ -36,25 +36,6 @@ test("Runewords get sorted by name", () => {
   runewordSortTest([...runewordsController.runewords], "name");
 });
 
-test("Runewords can be filtered by level", () => {
-  const maxRuneLevel = 30;
-  runewordsController.maxLevelFilter(maxRuneLevel);
-  const runewords = [...runewordsController.runewords];
-
-  let i = 0;
-  for (i = 0; i < runewords.length; i++) {
-    const word = runewords[i];
-    if (word.level <= maxRuneLevel) {
-      expect(word.filtered).toBe(false);
-    } else {
-      expect(word.filtered).toBe(true);
-    }
-  }
-
-  const filteredList = runewords.filter((word) => word.filtered === false);
-  expect(filteredList.length).toBe(20);
-});
-
 test("Runewords can be filtered by number of sockets", () => {
   // UI will probably be a radio of 1-6 as options
   const showSockets = 6;
@@ -64,15 +45,8 @@ test("Runewords can be filtered by number of sockets", () => {
   let i = 0;
   for (i = 0; i < runewords.length; i++) {
     const word = runewords[i];
-    if (word.sockets === showSockets) {
-      expect(word.filtered).toBe(false);
-    } else {
-      expect(word.filtered).toBe(true);
-    }
+    expect(word.sockets).toBe(6);
   }
-
-  const filteredList = runewords.filter((word) => word.filtered === false);
-  expect(filteredList.length).toBe(5);
 });
 
 test("App can show only sword runewords (filter by itemType)", () => {
@@ -84,15 +58,8 @@ test("App can show only sword runewords (filter by itemType)", () => {
   let i = 0;
   for (i = 0; i < runewords.length; i++) {
     const word = runewords[i];
-    if (word.itemType.includes(filterValue)) {
-      expect(word.filtered).toBe(false);
-    } else {
-      expect(word.filtered).toBe(true);
-    }
+    expect(word.itemType.includes(filterValue)).toBe(true);
   }
-
-  const filteredList = runewords.filter((word) => word.filtered === false);
-  expect(filteredList.length).toBe(14);
 });
 
 test("App can get a clean sorted list of all itemTypes", () => {
